3a3fb5ef0167be17ab10ece0eb4d82de6b2de162
Albert Vaca <albertvaka@gmail.com>
2014-01-25 15:23:54 +0100

    Added main scene with basic functionality

---
+++ Assets/Antagonism/Scripts/RestartOnOutOfScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartOnOutOfScreen : MonoBehaviour {
+
+	private float screenEdge;
+
+	void Start () {
+		screenEdge = Camera.main.ViewportToWorldPoint(new Vector3(1,0,1)).y - 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < screenEdge) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
---
+++ Assets/CameraScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	
+	public float speed = 3.5f;
+	public float distanceBetweenPlatforms = 4;
+	public GameObject platformPrefab;
+	private float nextPlatformX = 0, nextPlatformY = 0;
+	private float minY = 0, maxY = 100;
+	public GameObject firewall;
+
+
+	void Start () {
+		//Set firewall position
+		Vector3 leftBorder = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0,0.5f,1));
+		firewall.transform.position = leftBorder;
+
+		nextPlatformY = leftBorder.y - 5;
+
+		//Read screen geometry (TODO: Update on resize) 
+		maxY = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(1,1,1)).y - 5;
+		minY = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(1,0,1)).y + 5;
+
+	}
+	
+	void Update () {
+		//Scroll camera
+		Vector3 position = this.transform.position;
+		position.x += speed*Time.deltaTime;
+		speed += 0.01f*Time.deltaTime;
+		this.transform.position = position;
+
+		//Create new platforms
+		if (position.x > nextPlatformX) {
+			nextPlatformX = position.x + 2*distanceBetweenPlatforms;
+			Vector3 rightBorder = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(1,0.5f,-position.z));
+			rightBorder.x += distanceBetweenPlatforms;
+			nextPlatformY = nextPlatformY + Random.Range(-5,5);
+			nextPlatformY = Mathf.Clamp(nextPlatformY, minY, maxY);
+			rightBorder.y = nextPlatformY;
+			GameObject.Instantiate( platformPrefab, rightBorder, this.transform.rotation);
+		}
+
+	}
+
+}
+
+
+
+
