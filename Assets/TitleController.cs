17f0ce4f655b5e7632422adb84dbc3e6392dcf75
Albert Vaca <albertvaka@gmail.com>
2014-01-26 14:01:32 +0100

    Title screen and dead screen

---
+++ Assets/TitleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : MonoBehaviour {
+
+	void Update () {
+		
+
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown ("Fire1")) {
+			
+			Application.LoadLevel(Application.loadedLevel+1);
+			
+		}
+	}
+}
